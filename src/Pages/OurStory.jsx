319cabce83af79aa0045cb9926dcfb9586acc637
joshuakyayi256 <kyayijoshua@gmail.com>
2024-02-16 14:14:17 +0300

    edits

---
+++ src/Pages/OurStory.jsx
@@ -0,0 +1,12 @@
+
+
+export default function OurStory() {
+  return (
+    <div className="text-center">
+      <h1>Our Story</h1>
+      <p>
+        VivaCity Candles And Bath Salts was founded in 2020 by a group of friends who wanted to create a line of products that would help people relax and find peace in their everyday lives. Our candles and bath salts are made from the highest quality ingredients and are designed to help you find your inner peace.
+      </p>
+    </div>
+  );
+}
